d20a4ce43556ca6bab673733691f39a8a8caee0c
Klint Tiley <kctiley@gmail.com>
2015-11-13 14:20:33 -0700

    CORS now installed.Added to registry

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('dotenv').load({silent: true})
+require('dotenv').load({silent: true});
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', routes);
 app.use('/users', users);
